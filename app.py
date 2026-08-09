ccbe643a8e8c55043447f79652cb259a34234b6d
Alla Yaswanth <allayaswanth25@gmail.com>
2026-08-09 15:35:45 +0530

    Update YOLO application

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def video_feed():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))  # Use Render's port
-    app.run(host="0.0.0.0", port=port, debug=False)  # Expose externally for Render
+    app.run(host="0.0.0.0", port=port, debug=False)  # Expose externally for Render 
